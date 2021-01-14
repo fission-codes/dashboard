134aba9d75663110acd6b801d2e4bfd515f59a95
Philipp Krüger <philipp.krueger1@gmail.com>
2021-01-14 21:22:23 +0100

    Better checkbox and focus indicator styling

--- src/Css/Tailwind.js
+++ src/Css/Tailwind.js
@@ -52,6 +52,7 @@ export default {
       boxShadow: {
         "inner-outline": "inset 0 0 0 2px rgba(100, 70, 250, .2)", // purple 20%
         "outline": "0 0 0 2px rgba(100, 70, 250, 0.4)", // purple 40%
+        "outline-light": "0 0 0 2px rgba(255, 255, 255, 0.8)",
       },
       
     },
@@ -68,6 +69,7 @@ export default {
       maxWidth: [ 'responsive' ],
       display: [ 'dark' ],
       textDecoration: [ 'dark' ],
+      boxShadow: [ 'dark' ],
     }
   }
 }
