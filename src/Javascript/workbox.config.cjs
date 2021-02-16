4b6b0ee0f84bf0d864e4823f993bbfc2ee035e4d
Philipp Krüger <philipp.krueger1@gmail.com>
2021-02-16 21:37:47 +0100

    Add a service worker and manifest files

---
+++ src/Javascript/workbox.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    cacheId: "fission-suite/dashboard",
+    clientsClaim: true,
+    globDirectory: "build/",
+    globPatterns: [ "**/*" ],
+    inlineWorkboxRuntime: true,
+    navigateFallback: "index.html",
+    runtimeCaching: [
+      { urlPattern: /^https:\/\/cdnjs\./, handler: "StaleWhileRevalidate" }
+    ],
+    skipWaiting: true,
+    sourcemap: false,
+    swDest: "build/service-worker.js"
+  };
+  
