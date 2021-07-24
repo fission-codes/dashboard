b4829c20d9a4c73096e0dd639f81d7f6f323e11c
Philipp Krüger <philipp.krueger1@gmail.com>
2021-07-24 19:00:57 +0200

    Nevermind! There's a solution. Except for fonts

--- src/Book/Book.elm
+++ src/Book/Book.elm
@@ -1,13 +1,20 @@
 module Book exposing (main)
 
 import ChapterCommon exposing (chapterCommon)
-import ElmBook exposing (withChapters)
+import Css.Global
+import ElmBook exposing (withChapters, withThemeOptions)
 import ElmBook.ElmCSS exposing (Book, book)
+import ElmBook.ThemeOptions
+import Tailwind.Utilities
 
 
 main : Book ()
 main =
     book "Book"
+        |> withThemeOptions
+            [ ElmBook.ThemeOptions.globals
+                [ Css.Global.global Tailwind.Utilities.globalStyles ]
+            ]
         |> withChapters
             [ chapterCommon
             ]
