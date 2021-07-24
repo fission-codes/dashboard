04f8bd0b999078662b9ccdd222235823f4717767
Philipp Krüger <philipp.krueger1@gmail.com>
2021-07-24 20:18:48 +0200

    More design, updated elm-book :tada:

--- src/Book/Chapter/Common.elm
+++ src/Book/Chapter/Common.elm
@@ -3,6 +3,7 @@ module Chapter.Common exposing (it)
 import Css
 import ElmBook.Chapter exposing (chapter, render, withComponentList)
 import ElmBook.ElmCSS exposing (Chapter)
+import FeatherIcons
 import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes as A
 import Tailwind.Utilities as Tw
@@ -12,6 +13,9 @@ import View.Common
 content : String
 content =
     """
+> Keep in mind that this book doesn't have fonts loaded, so
+that's why some things look... *off*
+
 ## Buttons
 
 There is view function for buttons in `View.Common` with this signature:
@@ -36,8 +40,16 @@ property:
 
 <component with-label="View.Common.primaryButtonStyle" />
 
-Use the primary style for the *main* and *only* next
+Use the primary style for the *main* and *only*, next
 *non-destructive* action.
+
+<component with-label="View.Common.primaryDangerButtonStyle" />
+
+Use the primary danger style for the *main* and *only*, but
+*destructive* action. Use it for primary actions which can't
+be undone.
+(But prefer to avoid this style if possible by
+providing an 'undo' action.)
 
 <component with-label="View.Common.secondaryButtonStyle" />
 
@@ -46,9 +58,20 @@ aren't main.
 
 <component with-label="View.Common.uppercaseButtonStyle" />
 
-Use the uppercase style for basic button actions. You can
-use it when there are multiple equally-important options,
-e.g. settings screens.
+Use the uppercase style for *non-destrictive*, basic button
+actions. You can use it when there are multiple
+equally-important options, e.g. settings screens.
+
+<component with-label="View.Common.dangerButtonStyle" />
+
+Use the danger style for similar cases to the uppercase
+style, but for *destructive* actions.
+
+
+### Typography
+
+Generally, buttons should use title case (except for the
+uppercase style).
 
 
 ### Width
@@ -80,6 +103,37 @@ re-trigger the action while it's loading).
 > "Gotcha" in the mean time: The uppercase button has an
 empty list set as "spinnerStyle".
 
+
+### Icons
+
+Buttons can be decorated with `FeatherIcons`.
+
+Consider using them to improve recognition.
+
+<component with-label="Iconized" />
+
+
+## Warnings
+
+Sometimes, you want to give feedback to the user that an
+action failed unexpectedly or cannot be performed (e.g. due
+to input validation).
+
+For these cases, we support a `warning` view function:
+
+```elm
+import Html.Styled exposing (text, br)
+import View.Common
+
+View.Common.warning
+    [ text "We're sorry, something went wrong unexpectedly."
+    , br [] []
+    , text "Please contact our support!"
+    ]
+```
+
+<component with-label="Warning" />
+
 """
 
 
@@ -90,11 +144,17 @@ it =
             [ ( "View.Common.primaryButtonStyle"
               , View.Common.button primary
               )
+            , ( "View.Common.primaryDangerButtonStyle"
+              , View.Common.button primaryDanger
+              )
             , ( "View.Common.secondaryButtonStyle"
               , View.Common.button secondary
               )
             , ( "View.Common.uppercaseButtonStyle"
               , View.Common.button uppercase
+              )
+            , ( "View.Common.dangerButtonStyle"
+              , View.Common.button danger
               )
             , ( "Disabled"
               , sideBySide
@@ -114,6 +174,19 @@ it =
                         { secondary | disabled = True, isLoading = True }
                     , View.Common.button
                         { uppercase | disabled = True, isLoading = True }
+                    ]
+              )
+            , ( "Iconized"
+              , sideBySide
+                    [ View.Common.button { primary | icon = Just FeatherIcons.logIn }
+                    , View.Common.button { primaryDanger | label = "Break Encryption", icon = Just FeatherIcons.unlock }
+                    ]
+              )
+            , ( "Warning"
+              , View.Common.warning
+                    [ Html.text "We're sorry, something went wrong unexpectedly."
+                    , Html.br [] []
+                    , Html.text "Please contact our support!"
                     ]
               )
             ]
@@ -149,6 +222,17 @@ primary =
     }
 
 
+primaryDanger =
+    { label = "Do it."
+    , icon = Nothing
+    , disabled = False
+    , isLoading = False
+    , onClick = Nothing
+    , style = View.Common.primaryDangerButtonStyle
+    , spinnerStyle = [ View.Common.primaryButtonLoaderStyle ]
+    }
+
+
 secondary =
     { label = "Go back"
     , icon = Nothing
@@ -168,4 +252,15 @@ uppercase =
     , onClick = Nothing
     , style = View.Common.uppercaseButtonStyle
     , spinnerStyle = []
+    }
+
+
+danger =
+    { label = "Delete Something"
+    , icon = Nothing
+    , disabled = False
+    , isLoading = False
+    , onClick = Nothing
+    , style = View.Common.dangerButtonStyle
+    , spinnerStyle = [ View.Common.primaryButtonLoaderStyle ]
     }
