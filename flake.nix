a3997a821d4e64169b9ae774d2f31ac7efc12eb2
Steven Vandevelde <icid.asset@gmail.com>
2023-02-20 14:42:30 +0100

    Use unstable Nix set and update cachix/install-nix-action

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   # ======
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,7 +17,6 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
-      config = { allowBroken = true; };
       name = "fission-codes/dashboard";
       shell = ./shell.nix;
     };
